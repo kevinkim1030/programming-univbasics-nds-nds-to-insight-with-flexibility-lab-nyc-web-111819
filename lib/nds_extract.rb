04723f1fcc5bbea1c95482890acfa3cfbb81dd74
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-15 19:18:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -65,6 +65,7 @@ end
 
 
 def gross_per_studio(collection)
+  
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
