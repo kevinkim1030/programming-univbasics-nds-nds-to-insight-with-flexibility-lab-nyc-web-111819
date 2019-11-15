ab29cccda2cad84202b6f5cc6a7dc7a5cc90c4f9
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-15 19:45:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -68,8 +68,7 @@ def gross_per_studio(collection)
   i = 0 
   new_hash = {}
   while i < collection.length do 
-    studio_name = collection[i][:studio]
-    new_hash = {studio_name => collection[i][:worldwide_gross]}
+    new_hash = {collection[i] => collection[i][:worldwide_gross]}
     binding.pry 
     i += 1 
   end
